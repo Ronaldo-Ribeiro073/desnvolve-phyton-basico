9242c9483bbb6c00903a25f73a67dbdcb768dad6
Ronaldo-Ribeiro073 <156025026+Ronaldo-Ribeiro073@users.noreply.github.com>
2024-06-18 10:53:24 -0300

    Add files via upload

---
+++ modulo2/aula2_questao4.py
@@ -0,0 +1,11 @@
+saldo = 500
+juros = 1.01
+print("rendimento1")
+print(saldo*juros)
+saldo = saldo*juros
+print("rendimento2")
+print(saldo*juros)
+saldo = saldo*juros
+print("rendimento3")
+print(saldo*juros)
+saldo = saldo*juros
