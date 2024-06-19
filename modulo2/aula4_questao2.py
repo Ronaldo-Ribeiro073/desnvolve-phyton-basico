a96dc77c149280db7f633a11e76f42ea24560e9d
Ronaldo-Ribeiro073 <156025026+Ronaldo-Ribeiro073@users.noreply.github.com>
2024-06-19 14:48:08 -0300

    Update aula4_questao2.py

--- modulo2/aula4_questao2.py
+++ modulo2/aula4_questao2.py
@@ -1,11 +1,11 @@
-#2) Leia um valor inteiro correspondente a uma temperatura em graus Fahrenheit e apresente-a convertida em graus Celsius.
-#Fórmula de conversão: C = (F - 32) * (5/9), sendo C o valor em graus Celsius e F o valor em Fahrenheit. ]
-#Antes de imprimir, converta o valor em Celsius para inteiro. A mensagem deve estar formatada da seguinte maneira:
-
+""" 2) Leia um valor inteiro correspondente a uma temperatura em graus Fahrenheit e apresente-a convertida em graus Celsius.
+Fórmula de conversão: C = (F - 32) * (5/9), sendo C o valor em graus Celsius e F o valor em Fahrenheit. ]
+Antes de imprimir, converta o valor em Celsius para inteiro. A mensagem deve estar formatada da seguinte maneira:
+"""
 #86 graus Fahrenheit são 30 graus Celsius.
 
 # F representa Fahrenheit e C representa Celsius
-#Leitura da temperatura em Fahrenheit
+#Leitura da temperatura em Fahrenheit 
 F = int(input("Digite a temperatura em farenheith: "))
 C = (F - 32) * (5/9)
 print(f"A temperatura fornecida(F) é {C}° graus Celsius")
